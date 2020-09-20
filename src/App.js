2d17199dd978a5a03d3dcb1e6bbcc3034f55830a
Szabó Bence <szabo.bence@anteus.hu>
2020-09-20 16:42:53 +0200

    refakt: csak a térkép jelenik meg a teljes oldalon

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import React, {useState} from 'react';
-import {Col, Container, Row} from "react-bootstrap";
-import Header from "./components/Header";
-import Runners from "./components/Runners";
+import React from 'react';
 import Balaton from "./components/Balaton";
 
 
 const App = () => {
-
   return (
-    <Container fluid>
-      {/*<Header/>*/}
-      <Row>
-        <Col md={12}>
-          <Balaton />
-        </Col>
-        {/*<Col md={5}>*/}
-        {/*  <Runners/>*/}
-        {/*</Col>*/}
-      </Row>
-    </Container>
+    <Balaton/>
   );
 };
 
