9f9676ead9d0c2b713858493144ab2fa35dff31d
Szabó Bence <szabo.bence@anteus.hu>
2020-09-20 16:37:54 +0200

    Updates

---
+++ precache-manifest.7e6f1e0bd3c3b78ac98a72daed9d89b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ec6a8f8fe2727f198fb04159b13cead",
+    "url": "/ub2020anteus/index.html"
+  },
+  {
+    "revision": "6b9d94c358698f9d9509",
+    "url": "/ub2020anteus/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "6b9d94c358698f9d9509",
+    "url": "/ub2020anteus/static/js/2.f5907796.chunk.js"
+  },
+  {
+    "revision": "17dffa516c2ca25cecdb67ecbc5e1c6e",
+    "url": "/ub2020anteus/static/js/2.f5907796.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3c995c75b3df2a5fdd5",
+    "url": "/ub2020anteus/static/js/main.b5b843a3.chunk.js"
+  },
+  {
+    "revision": "674933faa1b5dae8f325",
+    "url": "/ub2020anteus/static/js/runtime-main.34eaa127.js"
+  }
+]);
